09ee104606c047ee88c3ff1d27454ecf53b8d60e
Paul <paul58914080@gmail.com>
2018-12-29 13:52:59 +0530

    Combined all tests as one for reporting

--- projects/todo-header/karma.conf.js
+++ projects/todo-header/karma.conf.js
@@ -17,7 +17,7 @@ module.exports = function (config) {
     },
     coverageIstanbulReporter: {
       dir: require('path').join(__dirname, '../../coverage/todo-header'),
-      reports: ['html', 'lcovonly', 'text-summary'],
+      reports: ['html', 'lcovonly', 'text-summary', 'json'],
       fixWebpackSourcePaths: true,
       thresholds: {
         statements: 80,
