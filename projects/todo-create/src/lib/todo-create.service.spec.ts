3f964079b31efba852015a56678da587ac57a0e1
paul58914080 <paul58914080@gmail.com>
2018-12-29 19:44:45 +0530

    Implemented the service to create todo

--- projects/todo-create/src/lib/todo-create.service.spec.ts
+++ projects/todo-create/src/lib/todo-create.service.spec.ts
@@ -1,12 +1,36 @@
 import {TestBed} from '@angular/core/testing';
 
 import {TodoCreateService} from './todo-create.service';
+import {HttpClientTestingModule, HttpTestingController} from '@angular/common/http/testing';
+import {Todo} from './todo';
 
 describe('TodoCreateService', () => {
-  beforeEach(() => TestBed.configureTestingModule({}));
+  let httpClientSpy: { get: jasmine.Spy };
+  let todoCreateService: TodoCreateService;
+  let httpMock: HttpTestingController;
+
+  beforeEach(() =>
+    TestBed.configureTestingModule({
+      imports: [HttpClientTestingModule],
+    })
+  );
+
+  beforeEach(() => {
+    httpClientSpy = jasmine.createSpyObj('HttpClient', ['get']);
+    todoCreateService = TestBed.get(TodoCreateService);
+    httpMock = TestBed.get(HttpTestingController);
+  });
 
   it('should be created', () => {
-    const service: TodoCreateService = TestBed.get(TodoCreateService);
-    expect(service).toBeTruthy();
+    expect(todoCreateService).toBeTruthy();
+  });
+
+  it('should save the todo', () => {
+    const todo: Todo = {completed: false, title: 'Watch Game of Thrones'};
+    todoCreateService.create(todo).subscribe();
+    const req = httpMock.expectOne(`/todo`, 'call to api for saving todo');
+    expect(req.request.method).toBe('POST');
+    req.flush({});
+    httpMock.verify();
   });
 });
